b02f6978ae8e1b5935552bda55e29a8683c2725b
mateixe <mateixe@gmail.com>
2015-04-14 23:15:50 -0300

    implementing basic angularjs

--- TouchBike2000Experiments/scripts/touchBike2000.js
+++ TouchBike2000Experiments/scripts/touchBike2000.js
@@ -1,6 +1,32 @@
 ﻿(function () {
 
     angular.module("touchBike2000", [])
+        .directive("mainMenuHover", function () {
+
+            return {
+
+                restrict: "E",
+
+                link: function (scope, element, attr) {
+
+                    var over = false;
+
+                    element.on('mouseenter', function (event) {
+                        event.preventDefault();
+                        over = true;
+                        console.log(over);
+                    });
+
+                    element.on('mouseleave', function (event) {
+                        event.preventDefault();
+                        over = false;
+                        console.log(over);
+                    });
+                    
+                }
+            }
+
+        })
         .controller("MainCtrl", ["$scope", function ($scope) {
 
             var self = this;
