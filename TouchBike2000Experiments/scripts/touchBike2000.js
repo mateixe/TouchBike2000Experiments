0ae52d631aa34f7a67a236c1fd15bc0231d284b3
mateixe <mateixe@gmail.com>
2015-04-14 21:50:48 -0300

    importing angular to the project

---
+++ TouchBike2000Experiments/scripts/touchBike2000.js
@@ -0,0 +1,23 @@
+﻿(function () {
+
+    angular.module("touchBike2000", [])
+        .controller("MainCtrl", [function () {
+
+            console.log("hello seu trouxa");
+
+            var self = this;
+
+            model = {
+                "labels": {
+                    "formas-bascas":""
+                }
+            };
+
+            self.onHoverMenu = function (event) {
+                event.prevenDefault();
+                console.log("hover");
+            }
+
+        }]);
+
+})();
