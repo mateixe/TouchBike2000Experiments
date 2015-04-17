d9eec0828ea3fd24e48cdf3ee6e90604afc700d3
mateixe <mateixe@gmail.com>
2015-04-16 23:39:56 -0300

    trying to implement gestures

--- TouchBike2000Experiments/scripts/touchBike2000.js
+++ TouchBike2000Experiments/scripts/touchBike2000.js
@@ -9,15 +9,20 @@
 
                 link: function (scope, element, attr) {
 
+                    scope.fullLabel = element.contents;
+                    scope.overLabel = String(element.contents).split(" ", 3)[0];
+
                     element.on('mouseenter', function (event) {
                         event.preventDefault();
                         over = true;
+                        element.html = scope.overLabel;
                         element.removeClass('bike-2000-opacity-off').addClass('bike-2000-opacity-on');
                     });
 
                     element.on('mouseleave', function (event) {
                         event.preventDefault();
                         over = false;
+                        element.html = scope.fullLabel;
                         element.removeClass('bike-2000-opacity-on').addClass('bike-2000-opacity-off');
                     });
 
