f5b17274bcc4de65b96fc5689ddc781b78ef9ce6
mateixe <mateixe@gmail.com>
2015-04-23 22:38:04 -0300

    starting to implement navigation

--- TouchBike2000Experiments/scripts/touchBike2000.js
+++ TouchBike2000Experiments/scripts/touchBike2000.js
@@ -5,33 +5,43 @@
 
             return {
 
-                restrict: "E",
+                restrict: "A",
 
                 link: function (scope, element, attr) {
 
                     scope.fullLabel = element.contents;
+                    console.log(scope.fullLabel);
                     scope.overLabel = String(element.contents).split(" ", 3)[0];
 
                     element.on('mouseenter', function (event) {
                         event.preventDefault();
                         over = true;
-                        element.html = scope.overLabel;
                         element.removeClass('bike-2000-opacity-off').addClass('bike-2000-opacity-on');
                     });
 
                     element.on('mouseleave', function (event) {
                         event.preventDefault();
                         over = false;
-                        element.html = scope.fullLabel;
                         element.removeClass('bike-2000-opacity-on').addClass('bike-2000-opacity-off');
-                    });
-
-                    element.on('swipeleft', function (event) {
-                        event.preventDefault();
-                        element.removeClass('bike-2000-opacity-on').addClass('bike-2000-opacity-all');
                     });
                     
                 }
+            }
+
+        })
+        .directive("home", function () {
+            restrict: "E"
+        })
+        .directive("formasBascas", function () {
+
+            return {
+
+                restrict: "E",
+
+                link: function (scope,element,attr) {
+                    console.log('formas-bascas');
+                }
+
             }
 
         })
